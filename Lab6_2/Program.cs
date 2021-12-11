28e2400cefe1e3345b6af98d5bd2dc25ccd5345d
Антон Еремеев <kzanton_81@mail.ru>
2021-12-12 01:15:00 +0300

    Добавьте файлы проекта.

---
+++ Lab6_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Ввести с клавиатуры предложение.
+Предложение представляет собой слова, разделенные пробелом.
+Знаки препинания не используются.
+Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).");
+            Console.WriteLine("");
+            Console.Write("Введите предложение: ");
+                        
+            string str = Console.ReadLine().ToLower().Replace(" ", "");
+                        
+            char[] CharArray = str.ToCharArray();
+           
+            Array.Reverse(CharArray);
+            
+            string strReverse = new string(CharArray);
+
+            if (str == strReverse)
+            {
+                Console.WriteLine("Введенное предложение является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Введенное предложение НЕ является палиндромом");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
